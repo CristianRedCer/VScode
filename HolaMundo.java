640111090b854209ee5b96f8dda3a38a0059d5b8
Cristian Redondo Cervantes <credcer2212@g.educaand.es>
2025-11-26 19:41:28 +0100

    Añado suma

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,6 +1,11 @@
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("hola mundo");
+        int a, b, c;
+        a = 5;
+        b = 10;
+        c = a + b;
+        System.out.println("La suma de a y b es: " + c);
     }
     
 }
