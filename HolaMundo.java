00115ca0b3a1e2218a7bb122437743ebb2ff357a
Cristian Redondo Cervantes <credcer2212@g.educaand.es>
2025-12-02 20:04:42 +0100

    Añado Scanner y meto resta de los valores

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,11 +1,18 @@
+import java.util.Scanner;
 public class HolaMundo {
     public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+         int a, b, c,d; 
         System.out.println("hola mundo");
-        int a, b, c;
-        a = 5;
-        b = 10;
+        System.out.println("Ingrese un valor que quieras sumar y restar(Tiene que ser este el mayor de los 2): ");
+        a = teclado.nextInt();
+        System.out.println("Ingrese otro valor que quieras sumar y restar: ");
+        b = teclado.nextInt();
+        teclado.nextLine();
         c = a + b;
-        System.out.println("La suma de a y b es: " + c);
+        d = a - b;
+        System.out.println("La suma de " + a + " y " + b + " es: " + c);
+        System.out.println("La resta de " + a + " y " + b + " es: " + d);
     }
     
 }
